55c821119957837205a7cd3a101312bca91ee3bd
Manu Aguilar <holasoymanuaguilar@gmail.com>
2020-01-11 02:40:08 +0100

    Fix Problem 2 Test

--- problem2/test.py
+++ problem2/test.py
@@ -5,11 +5,11 @@ class TestProblem(unittest.TestCase):
 
     def test_10(self):
         self.assertEqual(problem.fiboEvenSum(10),44)
-    def test_1000(self):
+    def test_18(self):
         self.assertEqual(problem.fiboEvenSum(18),3382)
-    def test_49(self):
+    def test_23(self):
         self.assertEqual(problem.fiboEvenSum(23),60696)
-    def test_19564(self):
+    def test_43(self):
         self.assertEqual(problem.fiboEvenSum(43),350704366)
 
 if __name__ == "__main__":
